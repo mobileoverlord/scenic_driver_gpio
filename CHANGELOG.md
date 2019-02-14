ec9e4025d2954f501670f383d09cd9adf14a617c
Justin Schneck <jschneck@mac.com>
2019-02-14 12:18:08 -0500

    v0.2.0 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # ScenicDriverGPIO
 
+## v0.2.0
+
+* Enhancements
+  * Updated circuits_gpio to v0.4.0
+  * Set GPIO pull mode on open
+
 ## v0.1.0
 
 Initial release
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ScenicDriverGPIO.MixProject do
   def project do
     [
       app: :scenic_driver_gpio,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.7",
       description: description(),
       package: package(),
